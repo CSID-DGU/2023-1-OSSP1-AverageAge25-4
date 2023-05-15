08434efeb7823a9873eb91738f123a8df34147a7
parkjimin0618 <batmin0618@naver.com>
2023-05-15 17:20:50 +0900

    [Feat] add tokenize method

--- Backend/dgunotice/similar.py
+++ Backend/dgunotice/similar.py
@@ -14,3 +14,28 @@ model4_path = 'model/Mecab_dataset.model'
 def getDataSet():
     data_set = Notice.objects.values_list('title', flat=True)
     return data_set
+
+# 전처리
+def tokenize():
+    db_data = getDataSet()
+    db_data = pd.DataFrame(db_data, columns=['제목'])
+    db_data['제목'] = db_data['제목'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]", "")
+
+    stop_words = []
+    with open('stopword.txt', encoding='utf-8') as f:
+        for i in f:
+            stop_words.append(i.strip())
+
+    kkma = Kkma()
+
+    tokenized_data = []
+
+    for sentence in tqdm.tqdm(db_data['제목']):
+        tokenized_sentence = kkma.nouns(sentence)
+        stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stop_words]
+        tokenized_data.append(stopwords_removed_sentence)
+
+    return tokenized_data
+
+
+
