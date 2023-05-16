c38ba94ad5094efa1019fe524bc378dac53f79b4
JangWooJin1 <jangwoojin1@gmail.com>
2023-05-16 12:23:10 +0900

    Fix : LoginPage.js

--- Frontend/dongguk-notice-app/src/LoginPage.js
+++ Frontend/dongguk-notice-app/src/LoginPage.js
@@ -1,69 +1,32 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-function LoginPage() {
-    const moveToMain = useNavigate();
-    const moveToLogin = useNavigate();
+const Login = () => {
+  const [uid, setUid] = useState('');
+  const [phone, setPhone] = useState('');
+  const navigate = useNavigate();
 
-    function goMain() {
-        moveToMain('/mainPage/')
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/login/', { uid, phone });
+      if (response.status === 200) {
+        navigate('/mainPage');
+      } else {
+        // Handle login failure
+      }
+    } catch (error) {
+      // Handle error
     }
+  };
 
-    function goLogin() {
-        moveToLogin('')
-    }
+  return (
+    <div>
+      <input type="text" value={uid} onChange={(e) => setUid(e.target.value)} placeholder="UID" />
+      <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="Phone" />
+      <button onClick={handleLogin}>로그인</button>
+    </div>
+  );
+};
 
-    const [userID, setUserID] = useState("");
-    const [phoneNum, setPhoneNum] = useState("");
-
-    const onIDHandler = (event) => {
-        setUserID(event.currentTarget.value);
-    }
-
-    const onPhoneNumHandler = (event) => {
-        setPhoneNum(event.currentTarget.value);
-    }
-
-    const sendPost = () => {
-        axios.post('http://127.0.0.1:8000/login/', { uid : userID, phone : phoneNum })
-        .then((response) => {
-            console.log('Post sent successfully:', response.data);
-            if(response.data.status===200)
-            {
-                console.log("로그인 성공");
-                goMain();
-            }
-            else if(response.data.status===401)
-            {
-                console.log("로그인 실패");
-                goLogin();
-            }
-        })
-        .catch((error) => {
-        console.error('Error sending post:', error);
-        });
-    }
-
-    return (
-        <div style={{ 
-            display: 'flex', justifyContent: 'center', alignItems: 'center', 
-            width: '100%', height: '15vh'
-            }}>
-            <form style={{ display: 'flex', flexDirection: 'column'}}
-                
-            >
-                <label>UserID</label>
-                <input type='text' value={userID} onChange={onIDHandler}/>
-                <label>PhoneNum</label>
-                <input type='text' value={phoneNum} onChange={onPhoneNumHandler}/>
-                <br />
-                <button onClick={sendPost}>
-                    Login
-                </button>
-            </form>
-        </div>
-    )
-}
-
-export default LoginPage;
+export default Login;
