6d4cb46210f227ced785dbc959814a078ce5231f
PARKJIMIN <batmin0618@naver.com>
2023-05-24 16:00:32 +0900

    [Test] Test 3 models with 20 corpus to analyse

--- Background/tester.py
+++ Background/tester.py
@@ -5,7 +5,8 @@ from smtp import sendAll
 os_path = 'model/ko.bin'
 own_path = 'model/ko_own.bin'
 combined_path = 'model/ko_modified.bin'
-old_path = 'model/Kkma_dataset.model.bin'
+old_path = 'model/Kkma_dataset.model'
+test_path = 'model/ko_test.model'
 
 # Old -> type = 0
 # New -> type = 1
@@ -16,9 +17,12 @@ def printSim(keyword, type):
     elif type == 1:
         print("기존 프로젝트 모델 : ", getSimKeyOld(keyword, 5)) # 이전프로젝트
     elif type == 2:
-        print("개선 프로젝트 모델 : ", getSimKey(keyword, 5)) # 스킵
+        #print("개선 프로젝트 모델 : ", getSimKey(keyword, 5)) # 스킵
+        print("개선 프로젝트 모델 : ", getSimKeyPath(keyword, 5, test_path))
     elif type == 3:
         print("OS 모델 : ", getSimKeyPath(keyword, 5, os_path)) # os
+    elif type == 4:
+        print("Test 모델 : ", getSimKeyPath(keyword, 5, test_path))  # os
 
     else:
         print("put 0 or 1 in type")
@@ -46,7 +50,10 @@ def printTokenizedOld(data):
     return print(tokenized_data)
 
 # ================================================TEST CALL======================================================#
-texts = ["학점", "공모전", "취업", "채용", "등록금", "개학", "전과", "예비군"]
+texts = ["학점", "공모전", "취업", "채용", "등록금", "개학", "전과",
+         "예비군", "박람회", "중앙도서관", "반납", "취득", "동아리",
+         "복수전공", "휴학", "아코", "축제", "방학", "신청", "계절학기"]
+
 for text in texts:
     print("[", text, "] 에 대한 유사단어 5개 추출")
     for i in range (1,4):
