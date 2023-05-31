da9b75e199dcea5afc54ae09429e11c52d9cae2a
HH <seayenseo@gmail.com>
2023-05-31 18:42:28 +0900

    암호화, 필터링 모듈 추가

---
+++ Backend/test/SecurityModule.py
@@ -0,0 +1,61 @@
+import re
+from cryptography.fernet import Fernet
+
+
+#공백 제외 숫자/문자만 남도록
+#정규 표현식 사용
+def str_replace(input_str, form='[^\w]'):
+    #정규 표현식 입력
+    pattern = re.compile(form)
+    return pattern.sub('', input_str)
+
+#암호화 (ASCII)
+#대칭키 방식 Crytography.Fernet 모듈 사용 (AES 방식)
+#키 값 변경 설계 예정
+#키 관리 방식 정의 필요
+class Key:
+    def __init__(self):
+        self.key = ''
+        self.load_key();
+
+    #키 생성 (서버에 키 저장)
+    def generate_key(self):
+        new_key = Fernet.generate_key()
+        self.key = new_key
+
+        #키 생성 경로(저장 위치 정해야함)
+        path = 'mykey.key'
+        with open(path, 'wb') as mykey:
+            mykey.write(new_key)
+
+    #저장된 키 로드
+    def load_key(self):
+        path = 'mykey.key'
+        try:
+            with open(path, 'rb') as mykey:
+                self.key = mykey.read()
+        except FileNotFoundError:
+            print('key 파일이 존재하지 않습니다')
+
+    def encrypt(self, input):
+        f = Fernet(self.key)
+        encpt_input = f.encrypt(bytes(input, 'ascii'))
+        return encpt_input
+
+    def decrypt(self, input):
+        f = Fernet(self.key)
+        decpt_input = f.decrypt(input)
+        return decpt_input
+
+
+#사용법
+
+# keytest.generate_key() 키 생성 (서버 생성 시 최초 1회만 수동으로 실행/키 값 변경시 수행(추천 :6개월~1년))
+# 이미 키 값 생성해 놓아서 할 필요 없습니다.
+
+# keytest = Key() 객체 생성
+# keytest.encrypt(input) 암호화
+# keytest.decrypt(input) 복호화
+
+
+
